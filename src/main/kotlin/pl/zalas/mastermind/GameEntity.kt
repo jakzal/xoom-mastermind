b1c3fb84a8b0023ae3f714492f676aa6ee77d9c2
Jakub Zalas <jakub@zalas.pl>
2020-01-22 22:24:11 +0000

    Calculate feedback pins

--- src/main/kotlin/pl/zalas/mastermind/GameEntity.kt
+++ src/main/kotlin/pl/zalas/mastermind/GameEntity.kt
@@ -25,6 +25,23 @@ class GameEntity(val id: GameId) : EventSourced(), Game {
         fun isGuessSuccessful(guess: Code) = secret.pins == guess.pins
 
         fun hasLastMoveLeft() = guesses.size == moves - 1
+
+        fun exactHits(guess: Code) = secret.pins
+            .zip(guess.pins)
+            .filter { it.first == it.second }
+            .map { Feedback.Pin.EXACT_HIT }
+
+        fun colourHits(guess: Code) = secret.diff(guess).run {
+            val secretColours = first.countPins()
+            val guessColours = second.countPins()
+            secretColours
+                .map { minOf(it.value, guessColours.getOrDefault(it.key, 0)) }
+                .flatMap { (1..it).map { Feedback.Pin.COLOUR_HIT } }
+        }
+
+        private fun List<Code.Pin>.countPins() = groupBy { pin -> pin }.mapValues { it.value.size }
+
+        private fun Code.diff(guess: Code) = pins.zip(guess.pins).filter { it.first != it.second }.unzip()
     }
 
     companion object {
@@ -41,9 +58,15 @@ class GameEntity(val id: GameId) : EventSourced(), Game {
     }
 
     override fun makeGuess(guess: Code) = when {
-        state.isGuessSuccessful(guess) -> apply(listOf(GuessMade(id, guess, Feedback()), GameWon(id)))
-        state.hasLastMoveLeft() -> apply(listOf(GuessMade(id, guess, Feedback()), GameLost(id)))
-        else -> apply(GuessMade(id, guess, Feedback()))
+        state.isGuessSuccessful(guess) -> apply(listOf(GuessMade(id, guess, giveFeedback(guess)), GameWon(id)))
+        state.hasLastMoveLeft() -> apply(listOf(GuessMade(id, guess, giveFeedback(guess)), GameLost(id)))
+        else -> apply(GuessMade(id, guess, giveFeedback(guess)))
+    }
+
+    private fun giveFeedback(guess: Code): Feedback {
+        val exactHits = state.exactHits(guess)
+        val colourHits = state.colourHits(guess)
+        return Feedback(exactHits + colourHits)
     }
 
     private fun applyGameStarted(gameStarted: GameStarted) {
--- src/test/kotlin/pl/zalas/mastermind/MastermindTests.kt
+++ src/test/kotlin/pl/zalas/mastermind/MastermindTests.kt
@@ -9,8 +9,14 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import pl.zalas.mastermind.Code.Pin.*
+import pl.zalas.mastermind.Feedback.Pin.COLOUR_HIT
+import pl.zalas.mastermind.Feedback.Pin.EXACT_HIT
 import pl.zalas.mastermind.GameEvent.*
+import java.util.stream.Stream
 
 class MastermindTests {
 
@@ -49,8 +55,8 @@ class MastermindTests {
 
         shouldHaveRaisedEvents(
             GameStarted(gameId, secret, 12),
-            GuessMade(gameId, Code(RED, RED, RED, RED), Feedback()),
-            GuessMade(gameId, Code(RED, BLUE, YELLOW, BLUE), Feedback()),
+            GuessMade(gameId, Code(RED, RED, RED, RED), Feedback(EXACT_HIT)),
+            GuessMade(gameId, Code(RED, BLUE, YELLOW, BLUE), Feedback(EXACT_HIT, EXACT_HIT, EXACT_HIT, EXACT_HIT)),
             GameWon(gameId)
         )
     }
@@ -67,14 +73,47 @@ class MastermindTests {
 
         shouldHaveRaisedEvents(
             GameStarted(gameId, secret, 2),
-            GuessMade(gameId, Code(RED, RED, RED, RED), Feedback()),
+            GuessMade(gameId, Code(RED, RED, RED, RED), Feedback(EXACT_HIT)),
             GuessMade(gameId, Code(PURPLE, PURPLE, PURPLE, PURPLE), Feedback()),
             GameLost(gameId)
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideFeedbackCases")
+    fun `feedback is given on each move made`(secret: Code, guess: Code, expectedFeedback: Feedback) {
+        val gameId = GameId.generate()
+        val gameBoard = world.actorFor(Game::class.java, GameEntity::class.java, gameId)
+
+        gameBoard.startGame(secret, 12)
+        gameBoard.makeGuess(guess)
+
+        shouldHaveRaisedEvents(
+            GameStarted(gameId, secret, 12),
+            GuessMade(gameId, guess, expectedFeedback)
         )
     }
 
     private fun shouldHaveRaisedEvents(vararg events: GameEvent) {
         dispatcher.updateExpectedEventHappenings(events.size)
         assertEquals(events.toList(), dispatcher.events(), "Should have raised the following events")
+    }
+
+    companion object {
+        @JvmStatic
+        private fun provideFeedbackCases() = Stream.of(
+            Arguments.of(Code(RED, BLUE, YELLOW, BLUE), Code(PURPLE, PURPLE, PURPLE, PURPLE), Feedback()),
+            Arguments.of(Code(RED, BLUE, YELLOW, BLUE), Code(RED, PURPLE, PURPLE, PURPLE), Feedback(EXACT_HIT)),
+            Arguments.of(Code(RED, BLUE, YELLOW, BLUE), Code(RED, RED, RED, RED), Feedback(EXACT_HIT)),
+            Arguments.of(Code(RED, BLUE, YELLOW, BLUE), Code(PURPLE, BLUE, PURPLE, PURPLE), Feedback(EXACT_HIT)),
+            Arguments.of(Code(RED, BLUE, YELLOW, BLUE), Code(PURPLE, BLUE, PURPLE, BLUE), Feedback(EXACT_HIT, EXACT_HIT)),
+            Arguments.of(Code(RED, BLUE, YELLOW, BLUE), Code(RED, BLUE, YELLOW, PURPLE), Feedback(EXACT_HIT, EXACT_HIT, EXACT_HIT)),
+            Arguments.of(Code(RED, BLUE, YELLOW, BLUE), Code(PURPLE, RED, PURPLE, PURPLE), Feedback(COLOUR_HIT)),
+            Arguments.of(Code(RED, BLUE, YELLOW, BLUE), Code(PURPLE, RED, RED, RED), Feedback(COLOUR_HIT)),
+            Arguments.of(Code(RED, BLUE, YELLOW, BLUE), Code(RED, RED, PURPLE, PURPLE), Feedback(EXACT_HIT)),
+            Arguments.of(Code(RED, BLUE, YELLOW, BLUE), Code(BLUE, RED, BLUE, YELLOW), Feedback(COLOUR_HIT, COLOUR_HIT, COLOUR_HIT, COLOUR_HIT)),
+            Arguments.of(Code(RED, BLUE, RED, BLUE), Code(RED, RED, PURPLE, PURPLE), Feedback(EXACT_HIT, COLOUR_HIT)),
+            Arguments.of(Code(RED, BLUE, RED, BLUE), Code(RED, RED, BLUE, BLUE), Feedback(EXACT_HIT, EXACT_HIT, COLOUR_HIT, COLOUR_HIT))
+        )
     }
 }
