d168c602a0bbbfd9a3d7bb6fed3227fe6747d8d1
Jakub Zalas <jakub@zalas.pl>
2020-01-23 23:50:02 +0000

    Bootstrap the documentation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Mastermind
 
+Code-breaking game.
+
+Documentation: https://jakzal.github.io/mastermind-kotlin/
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import org.asciidoctor.gradle.jvm.AsciidoctorTask
+
 plugins {
     kotlin("jvm") version "1.3.61"
+    id("org.asciidoctor.jvm.convert") version "3.0.0"
 }
 
 group = "pl.zalas"
 version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
@@ -27,4 +31,20 @@ tasks {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks {
+    "asciidoctor"(AsciidoctorTask::class) {
+        setBaseDir("docs")
+        setSourceDir(file("docs"))
+        setOutputDir(file("build/docs"))
+        sources(delegateClosureOf<PatternSet> {
+            include("index.adoc")
+        })
+        outputs.upToDateWhen { false }
+        attributes(mapOf(
+            "source-highlighter" to "coderay",
+            "snippets" to file("build/generated-snippets")
+        ))
+    }
 }
