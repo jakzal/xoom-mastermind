d924ed0d8f7cae79a30eb68b0d05bbea81f3f8f6
Jakub Zalas <jakub@zalas.pl>
2020-12-08 12:13:48 +0000

    Update the shadow jar plugin (5.2.0 -> 6.1.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     application
     kotlin("jvm") version "1.4.21"
     id("org.asciidoctor.jvm.convert") version "3.3.0"
-    id("com.github.johnrengelman.shadow") version "5.2.0"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 group = "pl.zalas"
